50bf60537ded271f3308f44534d35b4cdecae22f
hexu <hexu_1985@sina.com>
2022-01-18 17:35:40 +0800

    add memory_leaks/suppressions

--- address_sanitizer/use_after_free/cxx/build.sh
+++ address_sanitizer/use_after_free/cxx/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-gcc -g -fsanitize=address -fno-omit-frame-pointer -o use_after_free use_after_free.cpp
+g++ -g -fsanitize=address -fno-omit-frame-pointer -o use_after_free use_after_free.cpp
