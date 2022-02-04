209813857b7131ba9ebaed9a3039bfeaf4572bdf
hexu <hexu_1985@sina.com>
2022-02-04 18:36:08 +0800

    update load_so_by_rpath

--- dynamic_library/load_so_by_rpath/origin/c/app/build_app.sh
+++ dynamic_library/load_so_by_rpath/origin/c/app/build_app.sh
@@ -2,6 +2,4 @@
 
 set -x
 gcc -Wall -I ../lib -c main.c
-#gcc main.o -Wl,-R../lib -L../lib -lmydynamiclib -o app
-#gcc main.o -Wl,-rpath,../lib -L../lib -lmydynamiclib -o app
 gcc main.o -Wl,-rpath,'$ORIGIN/../lib' -L../lib -lmydynamiclib -o app
